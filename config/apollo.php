1efc0f617ecdd11e7a9072ab9893b045ce413634
KalimeroMK <zbogoevski@gmail.com>
2025-02-25 18:13:00 +0100

    remove test key

--- config/apollo.php
+++ config/apollo.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'search_api_key' => env('APOLLO_SEARCH_API_KEY', '77-XtBZ72F0H_cC33HZeZw'),
+    'search_api_key' => env('APOLLO_SEARCH_API_KEY', ''),
     'enrichment_api_key' => env('APOLLO_ENRICHMENT_API_KEY', ''),
     'account_api_key' => env('APOLLO_ACCOUNT_API_KEY', ''),
     'base_uri' => env('APOLLO_BASE_URI', 'https://api.apollo.io/v1'),
